7f4285cca4c577889c9494918e00deb88fa0089e
Mike McLaughlin <mmclaughlin@rallydev.com>
2012-12-13 14:55:45 -0500

    Chooser allowes us to find a chosen Portfolio Item

--- App.js
+++ App.js
@@ -1,100 +1,137 @@
+// return all leaf stories of a hard-coded PI object ID
 
-var query = {
-    "__At": "current",
-    "_TypeHierarchy":"HierarchicalRequirement",
-    "Children":null,
-    "_ItemHierarchy":7689990600
-};
-var find = ["ObjectID","_UnformattedID", "Name","Release"];
-var queryString = Ext.JSON.encode(query);
+
 Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
-    layout: {
-        type: 'vbox',
-        align: 'stretch'
+    extend:'Rally.app.App',
+    componentCls:'app',
+    layout:{
+        type:'vbox',
+        align:'stretch'
     },
     items:[
-        {
-            xtype: 'panel',
-            layout: 'anchor',
-            border: true,
-            fieldDefaults: {
-                labelWidth: 40
-            },
-            defaultType: 'textfield',
-            bodyPadding: 5,
-            
-            
-            buttons: [
-                {
-                    xtype: 'rallybutton',
-                    text: 'Go',
-                    itemId: 'goButton'
-                }
-            ]
+
+        { // define a container to house the PI combo box selector
+            xtype:'container',
+            itemId:'piChooserContainer',
+            padding:'15 15 15 15' // top ? bottom left
         },
         {
-            xtype: 'panel',
-            itemId: 'gridHolder',
-            layout: 'fit',
-            height: 400
+            // panel where we will place the grid
+            xtype:'panel',
+            itemId:'piLeafStoryGridContainer',
+            layout:'fit',
+            height:400
         }
     ],
-    launch: function() {
-        var button = this.down('#goButton');
-        button.on('click', this.goClicked, this);
-    },
-    
-    goClicked: function(){
-        var transformStore = Ext.create('Rally.data.lookback.SnapshotStore', {
-            context: {
-                workspace: this.context.getWorkspace(),
-                project: this.context.getProject()
-            },
-            fetch: find,
-            rawFind: query,
-            autoLoad: true,
-            listeners: {
-                scope: this,
-                load: this.processSnapshots
+
+
+    // --- global variables ---
+    gPiStories:[], // array to capture the list of all leaf stories in the PI
+
+    gStoryGrid:undefined,
+
+    _outstandingQueries:0, // count of recursive queries called
+    // --- end global variables ---
+
+
+    launch:function () {
+
+        Ext.create('Rally.ui.dialog.ChooserDialog',{
+            artifactTypes:['PortfolioItem/Feature'],
+            autoShow:true,
+            title:'Choose Feature',
+            listeners:{
+                artifactChosen:function (selectedRecord) {
+                    Ext.Msg.alert('Chooser', selectedRecord.get('Name') + ' was chosen');
+                    this._getLeafStoriesInPi(selectedRecord.get('ObjectID'));
+                },
+                scope:this
             }
         });
-    },
 
-    
-    processSnapshots: function(store, records){
+
+    }, // end launch
+
+
+    processSnapshots:function (store, records) {
         var snapshotGrid = Ext.create('Rally.ui.grid.Grid', {
-            title: 'Snapshots',
-            store: store,
-            columnCfgs: [
+            title:'Snapshots',
+            store:store,
+            columnCfgs:[
                 {
-                    text: 'ObjectID', 
-                    dataIndex: 'ObjectID'
+                    text:'ObjectID',
+                    dataIndex:'ObjectID'
                 },
                 {
-                    text: 'Name', 
-                    dataIndex: 'Name'
+                    text:'Name',
+                    dataIndex:'Name'
                 },
                 {
-                    text: 'Project',
-                    dataIndex: 'Project' 
+                    text:'Project',
+                    dataIndex:'Project'
                 },
                 {
-                    text: '_UnformattedID',
-                    dataIndex: '_UnformattedID'
+                    text:'_UnformattedID',
+                    dataIndex:'_UnformattedID'
                 },
                 ,
                 {
-                    text: 'Release',
-                    dataIndex: 'Release'
+                    text:'Release',
+                    dataIndex:'Release'
                 }
             ],
-            height: 400
+            height:400
         });
-        
-        var gridHolder = this.down('#gridHolder');
+
+        var gridHolder = this.down('#piLeafStoryGridContainer');
         gridHolder.removeAll(true);
         gridHolder.add(snapshotGrid);
-    }
+    },
+
+
+    // reset the page's controls
+    _reset:function () {
+        this.gPiStories = [];
+        //this._outstandingQueries = 0;
+        this.down('#piLeafStoryGridContainer').removeAll();
+    },
+
+
+    // after PI selected, query for all its stories
+    _getLeafStoriesInPi:function (piObjectID) {
+        this._reset();
+
+        var query = {
+            "__At":"current",
+            "_TypeHierarchy":"HierarchicalRequirement",
+            "Children":null,
+            "_ItemHierarchy":piObjectID
+        };
+
+        // set query config info
+        var find = ["ObjectID", "_UnformattedID", "Name", "Release"];
+
+        var queryString = Ext.JSON.encode(query);
+
+        var context = this.getContext().getDataContext();
+        context.project = undefined;
+
+
+        var ssPiLeafStories = Ext.create('Rally.data.lookback.SnapshotStore', {
+            context:{
+                workspace:this.context.getWorkspace(),
+                project:this.context.getProject()
+            },
+            fetch:find,
+            rawFind:query,
+            autoLoad:true,
+            listeners:{
+                scope:this,
+                load:this.processSnapshots
+            }
+        });
+
+
+    } // end _getStoriesInPi
+
 });
